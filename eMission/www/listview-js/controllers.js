cecbf5a6a8db6d58b6583f79f8d092c86f25345c
jessica wong <jesca116@berkeley.edu>
2015-04-29 15:55:42 -0700

    combined branches, trying to get map to show

--- eMission/www/listview-js/controllers.js
+++ eMission/www/listview-js/controllers.js
@@ -21,56 +21,71 @@ angular.module('starter.controllers', ['ionic'])
 
     // code to get trips from most recent day only
     var db = window.sqlitePlugin.openDatabase({name: "TripSections.db", location: 2, createFromLocation: 1});
-    tripSectionDbHelper.getJSON(db, function(jsonTripList) {
-        $scope.$apply(function () {
-            //$scope.trips = tripSectionDbHelper.getUncommitedSections(jsonTripList);
-            var all_trips = tripSectionDbHelper.getUncommitedSections(jsonTripList);
-            var mr_trip = tripSectionDbHelper.getUncommitedSections(jsonTripList).pop();
-            var mr_trips = [mr_trip];
-            var today = new Date(mr_trip.startTime.date);
-            for (var i = 0; i < all_trips; i++) {
-              var trip = all_trips[i];
-              // hacky way to check if date is the same
-              var tripDate = new Date(trip.startTime.date)
-              if (tripDate.getMonth() == today.getMonth()) {
-                if (tripDate.getDay() == today.getDay()) {
-                  if (tripDate.getYear() == today.getYear()) {
-                    mr_trips.push(trip);
+      tripSectionDbHelper.getJSON(db, function(jsonTripList) {
+          $scope.$apply(function () {
+              //$scope.trips = tripSectionDbHelper.getUncommitedSections(jsonTripList);
+              var all_trips = tripSectionDbHelper.getUncommitedSections(jsonTripList);
+              var mr_trip = tripSectionDbHelper.getUncommitedSections(jsonTripList).pop();
+              var mr_trips = [mr_trip];
+              console.log("mr_trip" + JSON.stringify(mr_trip));
+              var today = new Date(mr_trip.startTime.date);
+              for (var i = 0; i < all_trips; i++) {
+                var trip = all_trips[i];
+                console.log("trip in all_trips " + JSON.stringify(trip))
+
+                // hacky way to check if date is the same
+                var tripDate = new Date(trip.startTime.date)
+                if (tripDate.getMonth() == today.getMonth()) {
+                  if (tripDate.getDay() == today.getDay()) {
+                    if (tripDate.getYear() == today.getYear()) {
+                      mr_trips.push(trip);
+                      console.log('finalized mr_trip array: ' + mr_trips);
+                    }
                   }
                 }
               }
-            }
-            $scope.trips = mr_trips;
-            $scope.date = "" + today.getMonth() + "/" + today.getDay() + "/" + today.getYear();
-        });
-    });
+              $scope.trips = mr_trips;
+              $scope.date = "" + today.getMonth() + "/" + today.getDay() + "/" + today.getYear();
+          });
+      });
+
+
+    $scope.notSingleOrLast = function(index, list) {
+      console.log('index' + index);
+      if (index == list.length-1) {
+        return false;
+      }
+      else {
+        return true;
+      }
+
+    };
 
     $scope.getDisplayName = function(item) {
-      var coordinatesString = item["trackPoints"][0]["coordinate"][1] + "," + item["trackPoints"][0]["coordinate"][0]
-      var xmlHttp = new XMLHttpRequest();
-      var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinatesString + "&key=AIzaSyD8BQ3-gIFLsqR324AmQfGK6wCSauVAcEo"
-      console.log(url)
-      xmlHttp.open("GET", url, false);
-      xmlHttp.send();
-      var address_components = JSON.parse(xmlHttp.response)["results"][0]["address_components"];
-      var name = ""
-      for (var i = 0; i < address_components.length; i++) {
-        var component = address_components[i]
-        var types = component["types"]
-        if (types.indexOf("neighborhood") > -1) {
-          name = component["short_name"];
-          break;
-        } else if (types.indexOf("establishment") > -1) {
-          name = component["short_name"];
-          break;
-        } else if (types.indexOf("locality") > -1) {
-          name = component["short_name"];
-          break;
-        }
+    var coordinatesString = item["trackPoints"][0]["coordinate"][1] + "," + item["trackPoints"][0]["coordinate"][0]
+    var xmlHttp = new XMLHttpRequest();
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinatesString + "&key=AIzaSyD8BQ3-gIFLsqR324AmQfGK6wCSauVAcEo"
+    console.log(url)
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send();
+    var address_components = JSON.parse(xmlHttp.response)["results"][0]["address_components"];
+    var name = ""
+    for (var i = 0; i < address_components.length; i++) {
+      var component = address_components[i]
+      var types = component["types"]
+      if (types.indexOf("neighborhood") > -1) {
+        name = component["short_name"];
+        break;
+      } else if (types.indexOf("establishment") > -1) {
+        name = component["short_name"];
+        break;
+      } else if (types.indexOf("locality") > -1) {
+        name = component["short_name"];
+        break;
       }
-      console.log(name)
-      return name
     }
+    return name
+  }
 
     /*
     var db = $cordovaSQLite.openDB({name: "TripSections.db"});
